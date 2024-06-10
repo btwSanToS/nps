9f45cf037f5010fb5a2415c97588909abd3e8514
btwSanToS <andrelmsantos.87@gmail.com>
2024-06-10 20:30:15 -0300

    First Commit Project

---
+++ components/HeaderStatus/HeaderStatus.vue
@@ -0,0 +1,7 @@
+<template><div>
+Teste
+</div>
+</template>
+<script>
+</script>
+<style src="./HeaderStatus.css"/>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <HeaderStatus />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
