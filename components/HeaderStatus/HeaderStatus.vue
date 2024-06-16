733684c0ebf9937f2d30cdfa3e9d7a3a9cef6356
btwSanToS <andrelmsantos.87@gmail.com>
2024-06-16 20:52:33 -0300

    concat projeto

--- components/HeaderStatus/HeaderStatus.vue
+++ components/HeaderStatus/HeaderStatus.vue
@@ -1,14 +1,14 @@
 <template>
-  <div :class="{'full-height': fullHeight}" class="header">
+  <div :class="{ 'full-height': fullHeight }" class="header">
     <img src="@/assets/images/logo.jpg" />
     <button @click="expand">Enviar Avaliação</button>
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-const fullHeight = ref(false)
+import { ref } from "vue";
+const fullHeight = ref(false);
 const expand = () => {
-  fullHeight.value = !fullHeight.value
-}
+  fullHeight.value = !fullHeight.value;
+};
 </script>
 <style src="./HeaderStatus.css" scoped />
