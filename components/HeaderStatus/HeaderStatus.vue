e49ec409b942c520b45c952724dc5583e3a159b6
btwSanToS <andrelmsantos.87@gmail.com>
2024-06-16 20:48:39 -0300

    Att Projeto

--- components/HeaderStatus/HeaderStatus.vue
+++ components/HeaderStatus/HeaderStatus.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="header">
-  <img src="@/assets/images/logo.jpg"/>
+  <div :class="{'full-height': fullHeight}" class="header">
+    <img src="@/assets/images/logo.jpg" />
+    <button @click="expand">Enviar Avaliação</button>
   </div>
 </template>
-<script></script>
-<style src="./HeaderStatus.css" scoped/>
+<script setup>
+import { ref } from 'vue'
+const fullHeight = ref(false)
+const expand = () => {
+  fullHeight.value = !fullHeight.value
+}
+</script>
+<style src="./HeaderStatus.css" scoped />
